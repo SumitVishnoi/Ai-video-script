28bc625ac1941a1712cafe841817a93c23f42797
Sumit Vishnoi <sumitvishnoi7014@gmail.com>
2025-09-21 19:03:56 +0530

    update

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -68,8 +68,7 @@ export const login = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: true,
-            sameSite: "None",
-            path: "/"
+            sameSite: "None"
         })
 
         return res.status(200).json({
@@ -87,8 +86,7 @@ export const logout = async (req, res) => {
         res.clearCookie("token", {
             httpOnly: true,
             secure: true,
-            sameSite: "None",
-            path: "/"
+            sameSite: "None"
         })
 
     return res.status(200).json({
