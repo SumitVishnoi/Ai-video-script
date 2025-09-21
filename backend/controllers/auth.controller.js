61d1024a94f16e0a2aafa0204d22dbf3e3b9ee35
Sumit Vishnoi <sumitvishnoi7014@gmail.com>
2025-09-21 17:20:32 +0530

    logout update

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -83,7 +83,11 @@ export const login = async (req, res) => {
 
 export const logout = async (req, res) => {
     try {
-        res.clearCookie("token")
+        res.clearCookie("token", {
+            httpOnly: true,
+            secure: true,
+            sameSite: "None"
+        })
 
     return res.status(200).json({
         message: "logout successfully"
